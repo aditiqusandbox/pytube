98e195f52273e7a0d4a62fbd49a9ec7e1558c8b0
aditiqusandbox <149617801+aditiqusandbox@users.noreply.github.com>
2024-01-21 20:34:48 -0800

    Update innertube.py

--- pytube/innertube.py
+++ pytube/innertube.py
@@ -220,7 +220,7 @@ _token_file = os.path.join(_cache_dir, 'tokens.json')
 
 class InnerTube:
     """Object for interacting with the innertube API."""
-    def __init__(self, client='ANDROID_CREATOR', use_oauth=False, allow_cache=True): # ANDROID_MUSIC
+    def __init__(self, client='ANDROID_MUSIC', use_oauth=False, allow_cache=True): 
         """Initialize an InnerTube object.
 
         :param str client:
